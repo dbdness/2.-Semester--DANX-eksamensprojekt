3bb5fecdb2201e8eb57683d34946898603d39554
Danny <danny.nlsn0@gmail.com>
2015-05-08 10:48:12 +0200

    Added Daniel's MainView

--- DanxExamProject/DanxExamProject/View/DanxMainView.xaml.cs
+++ DanxExamProject/DanxExamProject/View/DanxMainView.xaml.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -18,56 +15,82 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace DanxExamProject.View
+namespace DanxExamProject
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class DanxMainView : Page
+    public sealed partial class MainPage : Page
     {
-        private int _i;
-        readonly List<string> _newsList = new List<string>() { "Danx Bla", "Danx - ----", "NewText", "Dandandnandnadna", "Lawl", "Yay" };
+        DispatcherTimer t = new DispatcherTimer();
+        DispatcherTimer v = new DispatcherTimer();
+        List<string> Newlist = new List<string>();
+        List<string> ValueList = new List<string>();
+        List<string> ValueTextList = new List<string>(); 
+        private int _i = 0;
+        private int _v = 0;
+        private int _vt = 0;
 
-
-        public DanxMainView()
+        public MainPage()
         {
-
             this.InitializeComponent();
 
-            DispatcherTimer t = new DispatcherTimer();
-            t.Interval = new TimeSpan(0, 0, 3);
+            Newlist.Add("DANX WINS TENDER OF NORDIC WAREHOUSE FOR BSH");
+            Newlist.Add("DANX GROUP EXPANDS INTO THE BALTIC COUNTRIES");
+            Newlist.Add("CSR AT DANX");
+            Newlist.Add("HYUNDAI MOBIS");
+            ValueList.Add("EQUALITY");
+            ValueList.Add("QUALITY");
+            ValueList.Add("FLEXIBILITY");
+            ValueList.Add("CREATIVITY");
+            ValueList.Add("AVAILABILITY");
+            ValueList.Add("PRIDE");
+            ValueTextList.Add(@"We are all equals, performing different roles to achieve the same goal. 
+We treat our customers, partners and colleagues with the same respect that we want to achieve ourselves. ");
+            ValueTextList.Add("We strive for 100% in everything we do – in that way we ensure that our customers meet their customers’ high expectations. ");
+            ValueTextList.Add("Flexibility is our mindset and the key to our success.");
+            ValueTextList.Add("As pioneers we think out of the box and create solutions for our customers’ needs. We are never satisfied and are constantly looking for new ways to improve.");
+            ValueTextList.Add("We ensure our customers’ availability of spare parts through personal care and availability.");
+            ValueTextList.Add("We are proud of our customers, our company and our people - we take pride in everything we do.");
+
+
+            t.Interval = new TimeSpan(0,0,15);
             t.Start();
-            
-            t.Tick += TOnTick;
-            
-            
+            t.Tick += DanxTick;
+
+            v.Interval = new TimeSpan(0,0,10);
+            v.Start();
+            v.Tick += ValueTick;
+            v.Tick += ValueTextTick;
+
+
+
         }
 
-        private void TOnTick(object sender, object o)
+        private void ValueTextTick(object sender, object e)
         {
-            
-           ChangeBlock.Text = _newsList[_i];
-            if (_i != _newsList.Count-1) _i++;
+            ValueText.Text = ValueTextList[_vt];
+            if (_vt != ValueTextList.Count - 1) _vt++;
+            else _vt = 0;
+        }
+
+
+        private void ValueTick(object sender, object e)
+        {
+            ValueBlock.Text = ValueList[_v];
+            if (_v != ValueList.Count - 1) _v++;
+            else _v = 0;
+        }
+        private void DanxTick(object sender, object e)
+        {
+            NewsBlock.Text = Newlist[_i];
+            if (_i != Newlist.Count - 1) _i++;
             else _i = 0;
+
+        }
             
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Task.Delay(100);
-            Frame.Navigate(typeof (loggedIn));
-        }
-
-        private void AppBarButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof (loggedIn));
-        }
-
-       
-
-        
-        
 
 
     }
-}
+
