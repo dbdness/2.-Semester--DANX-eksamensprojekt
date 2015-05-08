d62362a378cdbcf6aabf2c84ee41d4bac9e7385c
Danny <danny.nlsn0@gmail.com>
2015-05-08 12:35:57 +0200

    Deleted and added view again

--- DanxExamProject/DanxExamProject/App.xaml.cs
+++ DanxExamProject/DanxExamProject/App.xaml.cs
@@ -77,7 +77,7 @@ namespace DanxExamProject
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(DanxMainView), e.Arguments);
+                rootFrame.Navigate(typeof(BasicPage1), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
