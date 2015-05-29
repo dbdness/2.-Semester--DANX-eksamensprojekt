5b3a27d2777fb0a1d41e76f6a81a71a2556fb773
Danny <danny.nlsn0@gmail.com>
2015-05-30 00:31:22 +0200

    Deleted .old project

--- DanxExamProject/DanxAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace DanxAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
